11d49b0da18f2a83082b471d92a61e79d720deb4
Pratik <pratikpatil2101997@gmail.com>
2020-12-10 12:52:43 +0530

    initial commit

---
+++ Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld{
+    public static void main(String args[]){
+        System.out.println("hello World...");
+        System.out.println("added new line");
+        int num = 100;
+
+    }
+}
